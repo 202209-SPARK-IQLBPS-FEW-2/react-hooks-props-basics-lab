a82f9168415432c386cb9d5be2d0c65fb6a24a2d
MaryyamSalah <maryyamslah20@gmail.com>
2022-12-08 15:54:02 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ import Links from "./Links";
 function About(props) {
   return (
     <div id="about">
+      
       <h2>About Me</h2>
        {props.bio || props.bio ===" "? <p>{props.bio} </p>: null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
